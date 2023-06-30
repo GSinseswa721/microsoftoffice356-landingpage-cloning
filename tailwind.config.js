5d6cbd06d4f520dcbd04159f5a37a555b04b7b50
Gloria Sinseswa <glorianiyonkurusinseswa@gmail.com>
2023-06-30 23:24:57 +0300

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], 
+  theme: {
+    extend: {
+      colors:{
+        "color-primary": "white",
+      }
+    },
+    
+  },
+  plugins: [],
+}
+
